a25aa0dae877f27946eb62bf5854141b14b918db
cleanbot-hub <0x000000e@gmail.com>
2023-06-20 15:07:04 +0900

    main

---
+++ study0620/static/js/script_array5.js
@@ -0,0 +1,30 @@
+window.onload=function(){
+    
+    let pic1=document.getElementById("pic1");
+    pic1.addEventListener("click", same_search);
+
+    let pic2=document.getElementById("pic2");
+    pic2.addEventListener("click", same_search);
+
+    let pic3=document.getElementById("pic3");
+    pic3.addEventListener("click", same_search);
+
+    let pic4=document.getElementById("pic4");
+    pic4.addEventListener("click", same_search);
+
+    let pic5=document.getElementById("pic5");
+    pic5.addEventListener("click", same_search);
+
+    let pic6=document.getElementById("pic6");
+    pic6.addEventListener("click", same_search);
+
+    let pic7=document.getElementById("pic7");
+    pic7.addEventListener("click", same_search);
+
+    let pic8=document.getElementById("pic8");
+    pic8.addEventListener("click", same_search);
+
+
+}
+
+function same_search();
