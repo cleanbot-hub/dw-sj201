9d0dd1466f3129615b1fbc06217ad6e40de0e5ec
cleanbot-hub <0x000000e@gmail.com>
2023-06-20 11:05:09 +0900

    main

---
+++ study0620/static/js/script_array3.js
@@ -0,0 +1,50 @@
+// 배열을 생성하는 방법 
+// 
+
+//1번째 방법  Array1 = new Array();
+//2번째 방법  var a = [1,2,3,4,5];
+ 
+// 배열의 크기 - array1.length
+
+//
+
+
+// var number =[10,32,54,65,34,59,60];
+// for(var i=0; i<number.length; i++){
+//     document.write(number[i] + "<br>");
+// }
+
+// var number2 = new Array();
+// // 5개 정수 입력하여 배열에 저장하기 
+
+// for(var i=0; i<5; i++){
+//     number2.push(prompt());
+   
+// }
+
+
+// var names= ["하지원","한지민","전지현","손예진","한가인"];
+// for(var i=0; i<names.length; i++){
+//     document.write(names[i]+ "<br>");
+// }
+
+
+// 배열에 새로운 값 저장 할 때는 인덱스를 사용하면 안된다.
+// var number = new Array();
+// for(var i=1; i<100; i++){
+//     number.push(Math.floor(Math.random()*200)+1); // 1에서 200까지의 랜덤 값이 저장이 된다 
+// }
+
+// for( var i=0; i<number.length; i++){
+//     if(!(number[i]%3 == 0 || number[i]%5 == 0)) 
+//     // 같지 않다라는 연산자 사용 이 어려울수가 있다 제외할 데이터를 ! 붙혀준다. 
+//     // 
+//     document.write(number[i] + "<br>");
+// }
+
+
+// 
+
+
+
+
