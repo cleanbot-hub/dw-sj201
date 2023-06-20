714b904935216b423914c69e0248f8d41a568de5
cleanbot-hub <0x000000e@gmail.com>
2023-06-20 15:25:23 +0900

    main

--- study0620/static/js/script_array5.js
+++ study0620/static/js/script_array5.js
@@ -1,30 +1,17 @@
 window.onload=function(){
-    
-    let pic1=document.getElementById("pic1");
-    pic1.addEventListener("click", same_search);
 
-    let pic2=document.getElementById("pic2");
-    pic2.addEventListener("click", same_search);
+    let pic = document.getElementsByClassName("picture"); // 배열에 첫번째 주소
+    for(var i=0; i<pic.length; i++){ // 배열 
+        pic[i].addEventListener("click",same_search); // 8개의 td에 클릭 이벤트 등록 
+        pic[i].innerHTML=Math.floor(Math.random()*10)+1;
+    }
+        
+}
 
-    let pic3=document.getElementById("pic3");
-    pic3.addEventListener("click", same_search);
-
-    let pic4=document.getElementById("pic4");
-    pic4.addEventListener("click", same_search);
-
-    let pic5=document.getElementById("pic5");
-    pic5.addEventListener("click", same_search);
-
-    let pic6=document.getElementById("pic6");
-    pic6.addEventListener("click", same_search);
-
-    let pic7=document.getElementById("pic7");
-    pic7.addEventListener("click", same_search);
-
-    let pic8=document.getElementById("pic8");
-    pic8.addEventListener("click", same_search);
+function same_search(){
 
 
 }
 
-function same_search();
+
+
