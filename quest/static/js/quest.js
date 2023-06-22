6f57229116636e649920d2857e7879e09d0922a4
cleanbot-hub <0x000000e@gmail.com>
2023-06-22 18:58:14 +0900

    main

--- quest/static/js/quest.js
+++ quest/static/js/quest.js
@@ -1,27 +1,38 @@
 function start() {
   var num = document.getElementById("num").value;
-  var com1 = Math.floor(Math.random()*3)+1;
-  var split = num.split(',');
-  var numbers = split.map(function(item) {
-    return parseInt(item.trim());
-  });
- 
   var res = document.getElementById("result");
-  res.innerHTML = numbers.join(' ');
-  
-  for(var i=0; i<numbers.length; i++){
-  if(!isNaN(numbers[i])) {
-    alert("true");
-         }else{
-        alert("false");
-    
-        }
+  var res1 = document.getElementById("result1");
+
+  var glucoseLevel = parseInt(num);
+  res.innerHTML = num;
+
+  if (glucoseLevel >= 70 && glucoseLevel <= 100) {
+    res.innerHTML += " - 공복 혈당 (정상)";
+    if (glucoseLevel < 5.7) {
+      res1.innerHTML = "정상 수치보다 5.7% 미만입니다.";
+    } else {
+      res1.innerHTML = "글리코프로틴 혈색소 (HbA1c) 수치: " + glucoseLevel + "%";
     }
+  } else if (glucoseLevel >= 90 && glucoseLevel <= 140) {
+    res.innerHTML += " - 식후 2시간 혈당 (정상)";
+    if (glucoseLevel < 5.7) {
+      res1.innerHTML = "정상 수치보다 5.7% 미만입니다.";
+    } else {
+      res1.innerHTML = "글리코프로틴 혈색소 (HbA1c) 수치: " + glucoseLevel + "%";
+    }
+  } else if (glucoseLevel < 70) {
+    res.innerHTML += " - 저혈당 (저혈당)";
+    if (glucoseLevel < 5.7) {
+      res1.innerHTML = "정상 수치보다 5.7% 미만입니다.";
+    } else {
+      res1.innerHTML = "글리코프로틴 혈색소 (HbA1c) 수치: " + glucoseLevel + "%";
+    }
+  } else if (glucoseLevel > 140) {
+    res.innerHTML += " - 고혈당 (고혈당)";
+    if (glucoseLevel < 5.7) {
+      res1.innerHTML = "정상 수치보다 5.7% 미만입니다.";
+    } else {
+      res1.innerHTML = "글리코프로틴 혈색소 (HbA1c) 수치: " + glucoseLevel + "%";
+    }
+  }
 }
-
-
-
-
-
-
-
