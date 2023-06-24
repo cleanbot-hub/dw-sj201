de873e833af9552b78f655f0f8e6628cf0ddf0d6
cleanbot-hub <0x000000e@gmail.com>
2023-06-24 12:00:41 +0900

    빙고 게임

--- binggo/static/js/binggo.js
+++ binggo/static/js/binggo.js
@@ -1,6 +1,6 @@
 function start() {
     // Generate an array of random unique numbers between 1 and 50
-    var numbers = generateRandomNumbers(25, 1, 50);
+    var numbers = generateRandomNumbers(25, 1, 50); 
 
     // Clear the existing grid
     clearGrid();
@@ -8,12 +8,13 @@ function start() {
     // Create a 5x5 bingo grid and populate it with the generated numbers
     createBingoGrid(numbers);
 
+    // Update text content of the element with class "cell"
+    var cells = document.getElementsByClassName("cell");
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].innerText = "pressed";
+    }
+}
 
-   
-
-
-
-  }
 
 function generateRandomNumbers(count, min, max) {
     var numbers = [];
@@ -30,6 +31,7 @@ function clearGrid() {
     var cells = document.getElementsByClassName("cell");
     for (var i = 0; i < cells.length; i++) {
         cells[i].innerText = "";
+        cells[i].classList.remove("cell-selected");
     }
 }
 
@@ -42,7 +44,15 @@ function createBingoGrid(numbers) {
             index++;
             cells[i].addEventListener("click", function() {
                 this.classList.toggle("cell-selected");
+                this.innerText = "selected";
             });
         }
     }
 }
+
+
+// Reset button functionality
+var resetButton = document.getElementById("resetButton");
+resetButton.addEventListener("click", function() {
+    clearGrid();
+});
