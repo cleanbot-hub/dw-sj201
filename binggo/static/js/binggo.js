00de9a715d0e763a7adccf662fd9ac6646f7c91c
cleanbot-hub <0x000000e@gmail.com>
2023-06-26 21:30:37 +0900

    main

--- binggo/static/js/binggo.js
+++ binggo/static/js/binggo.js
@@ -1,18 +1,18 @@
 // window.addEventListener("load", start);
 
-var selectedCells = []; // Array to track selected cells
-var bingoCount = 0; // Count of achieved bingos
-var gameStarted = false; // Indicates whether the game has started
+var selectedCells = []; // 선택된 셀을 추적하기 위한 배열
+var bingoCount = 0; // 달성한 빙고의 개수
+var gameStarted = false; // 게임이 시작되었는지를 나타내는 변수
 
 function start() {
   var numbers = generateRandomNumbers(25, 1, 50);
   clearGrid();
   createBingoGrid(numbers);
-  bingoCount = 0; // Reset the bingo count
-  gameStarted = true; // Set gameStarted to true
+  bingoCount = 0; // 빙고 개수 초기화
+  gameStarted = true; // gameStarted를 true로 설정
 }
 
-// Generate random numbers within a given range without duplicates
+// 주어진 범위 내에서 중복되지 않는 무작위 숫자 생성
 function generateRandomNumbers(count, min, max) {
   var numbers = [];
   while (numbers.length < count) {
@@ -24,19 +24,19 @@ function generateRandomNumbers(count, min, max) {
   return numbers;
 }
 
-// Clear the grid
+// 그리드 초기화
 function clearGrid() {
   var cells = document.getElementsByClassName("cell");
   for (var i = 0; i < cells.length; i++) {
     cells[i].innerText = "";
     cells[i].classList.remove("cell-selected");
   }
-  selectedCells = []; // Reset selected cells array
-  bingoCount = 0; // Reset the bingo count
-  gameStarted = false; // Reset gameStarted to false
+  selectedCells = []; // 선택된 셀 배열 초기화
+  bingoCount = 0; // 빙고 개수 초기화
+  gameStarted = false; // gameStarted를 false로 설정
 }
 
-// Create the bingo grid
+// 빙고 그리드 생성
 function createBingoGrid(numbers) {
   var index = 0;
   var cells = document.getElementsByClassName("cell");
@@ -47,7 +47,7 @@ function createBingoGrid(numbers) {
       index++;
 
       cells[i].addEventListener("click", function () {
-        if (gameStarted) { // Check if the game has started
+        if (gameStarted) { // 게임이 시작되었는지 확인
           if (!this.classList.contains("cell-selected")) {
             this.classList.add("cell-selected");
             selectedCells.push(this.id);
@@ -58,24 +58,24 @@ function createBingoGrid(numbers) {
             checkBingo();
           }
         } else {
-          alert("Please start the game first!"); // Show alert if the game hasn't started
+          alert("먼저 게임을 시작해주세요!"); // 게임이 시작되지 않았을 때 알림 표시
         }
       });
     }
   }
 }
 
-// Check for bingos
+// 빙고 확인
 function checkBingo() {
   var lineBingos = [];
 
-  // Check rows, columns, vertical columns, and diagonals
+  // 행, 열, 대각선에 대한 빙고 확인
 
-  // ... (existing code for checking bingos)
+  // ... (빙고 확인을 위한 기존 코드)
 
-  // Check for 5-cell bingo based on selected cells
+  // 선택된 셀을 기반으로 5칸 빙고 확인
   if (selectedCells.length >= 5) {
-    // Check for horizontal bingos
+    // 가로 빙고 확인
     for (var row = 0; row < 5; row++) {
       var bingoCountInRow = 0;
       for (var col = 0; col < 5; col++) {
@@ -85,11 +85,11 @@ function checkBingo() {
         }
       }
       if (bingoCountInRow === 5) {
-        lineBingos.push("Row " + (row + 1));
+        lineBingos.push("행 " + (row + 1));
       }
     }
 
-    // Check for vertical bingos
+    // 세로 빙고 확인
     for (var col = 0; col < 5; col++) {
       var bingoCountInCol = 0;
       for (var row = 0; row < 5; row++) {
@@ -99,11 +99,11 @@ function checkBingo() {
         }
       }
       if (bingoCountInCol === 5) {
-        lineBingos.push("Column " + (col + 1));
+        lineBingos.push("열 " + (col + 1));
       }
     }
 
-    // Check for diagonal bingos
+    // 대각선 빙고 확인
     var diagonal1Bingo = true;
     var diagonal2Bingo = true;
     for (var i = 0; i < 5; i++) {
@@ -117,21 +117,21 @@ function checkBingo() {
       }
     }
     if (diagonal1Bingo) {
-      lineBingos.push("Diagonal 1");
+      lineBingos.push("대각선 1");
     }
     if (diagonal2Bingo) {
-      lineBingos.push("Diagonal 2");
+      lineBingos.push("대각선 2");
     }
   }
 
-  // Update bingo count and show alert
+  // 빙고 개수 업데이트 및 알림 표시
   if (lineBingos.length > bingoCount) {
     bingoCount = lineBingos.length;
     if (bingoCount < 5) {
-      alert("Bingo! " + bingoCount + " bingo achieved!");
+      alert("빙고! " + bingoCount + "개의 빙고가 달성되었습니다!");
     } else {
-      alert("Bingo! Game over! 5 bingos achieved!");
-      clearGrid(); // Reset the grid when the game is over
+      alert("빙고! 게임 종료! 5개의 빙고가 달성되었습니다!");
+      clearGrid(); // 게임이 종료되면 그리드를 초기화
     }
     return;
   }
