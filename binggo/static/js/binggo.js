63462af0594f55fedc228c1da2259c2a1db47c2d
cleanbot-hub <0x000000e@gmail.com>
2023-06-26 20:11:21 +0900

    main

--- binggo/static/js/binggo.js
+++ binggo/static/js/binggo.js
@@ -7,6 +7,7 @@ function start() {
   var numbers = generateRandomNumbers(25, 1, 50);
   clearGrid();
   createBingoGrid(numbers);
+  bingoCount = 0; // Reset the bingo count
 }
 
 // Generate random numbers within a given range without duplicates
@@ -26,7 +27,10 @@ function clearGrid() {
   var cells = document.getElementsByClassName("cell");
   for (var i = 0; i < cells.length; i++) {
     cells[i].innerText = "";
+    cells[i].classList.remove("cell-selected");
   }
+  selectedCells = []; // Reset selected cells array
+  bingoCount = 0; // Reset the bingo count
 }
 
 // Create the bingo grid
@@ -47,6 +51,7 @@ function createBingoGrid(numbers) {
         } else {
           this.classList.remove("cell-selected");
           selectedCells = selectedCells.filter((cell) => cell !== this.id);
+          checkBingo();
         }
       });
     }
@@ -57,69 +62,59 @@ function createBingoGrid(numbers) {
 function checkBingo() {
   var lineBingos = [];
 
-  // Check rows
-  for (var row = 0; row < 5; row++) {
-    var rowBingo = true;
+  // Check rows, columns, vertical columns, and diagonals
+
+  // ... (existing code for checking bingos)
+
+  // Check for 5-cell bingo based on selected cells
+  if (selectedCells.length >= 5) {
+    // Check for horizontal bingos
+    for (var row = 0; row < 5; row++) {
+      var bingoCountInRow = 0;
+      for (var col = 0; col < 5; col++) {
+        var cellId = "cell" + (row * 5 + col + 1);
+        if (selectedCells.includes(cellId)) {
+          bingoCountInRow++;
+        }
+      }
+      if (bingoCountInRow === 5) {
+        lineBingos.push("Row " + (row + 1));
+      }
+    }
+
+    // Check for vertical bingos
     for (var col = 0; col < 5; col++) {
-      var cellId = "cell" + (row * 5 + col + 1);
-      if (!selectedCells.includes(cellId)) {
-        rowBingo = false;
-        break;
+      var bingoCountInCol = 0;
+      for (var row = 0; row < 5; row++) {
+        var cellId = "cell" + (row * 5 + col + 1);
+        if (selectedCells.includes(cellId)) {
+          bingoCountInCol++;
+        }
+      }
+      if (bingoCountInCol === 5) {
+        lineBingos.push("Column " + (col + 1));
       }
     }
-    if (rowBingo) {
-      lineBingos.push("Row " + (row + 1));
-    }
-  }
 
-  // Check columns
-  for (var col = 0; col < 5; col++) {
-    var colBingo = true;
-    for (var row = 0; row < 5; row++) {
-      var cellId = "cell" + (row * 5 + col + 1);
-      if (!selectedCells.includes(cellId)) {
-        colBingo = false;
-        break;
+    // Check for diagonal bingos
+    var diagonal1Bingo = true;
+    var diagonal2Bingo = true;
+    for (var i = 0; i < 5; i++) {
+      var cellId1 = "cell" + (i * 5 + i + 1);
+      var cellId2 = "cell" + (i * 5 + (4 - i) + 1);
+      if (!selectedCells.includes(cellId1)) {
+        diagonal1Bingo = false;
+      }
+      if (!selectedCells.includes(cellId2)) {
+        diagonal2Bingo = false;
       }
     }
-    if (colBingo) {
-      lineBingos.push("Column " + (col + 1));
+    if (diagonal1Bingo) {
+      lineBingos.push("Diagonal 1");
     }
-  }
-
-  // Check vertical columns
-  for (var col = 0; col < 5; col++) {
-    var verticalBingo = true;
-    for (var row = 0; row < 5; row++) {
-      var cellId = "cell" + (col * 5 + row + 1);
-      if (!selectedCells.includes(cellId)) {
-        verticalBingo = false;
-        break;
-      }
+    if (diagonal2Bingo) {
+      lineBingos.push("Diagonal 2");
     }
-    if (verticalBingo) {
-      lineBingos.push("Vertical Column " + (col + 1));
-    }
-  }
-
-  // Check diagonals
-  var diagonal1Bingo = true;
-  var diagonal2Bingo = true;
-  for (var i = 0; i < 5; i++) {
-    var cellId1 = "cell" + (i * 5 + i + 1);
-    var cellId2 = "cell" + (i * 5 + (4 - i) + 1);
-    if (!selectedCells.includes(cellId1)) {
-      diagonal1Bingo = false;
-    }
-    if (!selectedCells.includes(cellId2)) {
-      diagonal2Bingo = false;
-    }
-  }
-  if (diagonal1Bingo) {
-    lineBingos.push("Diagonal 1");
-  }
-  if (diagonal2Bingo) {
-    lineBingos.push("Diagonal 2");
   }
 
   // Update bingo count and show alert
@@ -129,7 +124,8 @@ function checkBingo() {
       alert("Bingo! " + bingoCount + " bingo achieved!");
     } else {
       alert("Bingo! Game over! 5 bingos achieved!");
+      clearGrid(); // Reset the grid when the game is over
     }
-    return; // 종료
+    return;
   }
 }
