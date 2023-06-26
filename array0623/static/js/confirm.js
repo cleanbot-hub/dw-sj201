bbc0e0a55d8dba9c37146982becc72c4dd89188e
cleanbot-hub <0x000000e@gmail.com>
2023-06-26 15:05:19 +0900

    main

---
+++ array0623/static/js/confirm.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+    var drwNo = document.querySelector("#drwNo");
+    var btnDefault = document.querySelector("#btnDefault");
+    var btnSearch=document.querySelector("#btnSearch");
+
+
+    
+
+
+}
