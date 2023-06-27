7fef32c8507beda44d29514735cdbde20c77c3c0
cleanbot-hub <0x000000e@gmail.com>
2023-06-27 23:36:04 +0900

    main

--- study0628/js/script_test.js
+++ study0628/js/script_test.js
@@ -5,6 +5,9 @@ const fruit = [
   '퍼그', '파피용', '시바견', '비숑프리제', '스코티쉬테리어'
 ];
 
+
+
+
 let game_state = false;
 let path = './image/';
 let image_name = ['beagle.png', 'bichonfrize.png', 'bordercollie.png', 'chihuahua.png', 'dachshund.png',
@@ -19,11 +22,11 @@ let isSame = [];
 let selectImg = [];
 
 window.onload = function () {
-
   var start_bt = document.getElementById('start');
   start_bt.addEventListener('click', game_start);
   var count = document.getElementById('count');
   count.innerText = 0;
+  
 }
 
 function image_init() {
@@ -48,69 +51,14 @@ function image_init() {
 
 function game_start() {
   if (isStart) {
-    return;
+    // return;
   }
   image_init();
-  var td = document.getElementsByClassName('picture_box');
-  for (var i = 0; i < td.length; i++) {
-    td[i].addEventListener('click', compare_img);
-    isSame.push(false);
-  }
-//   setTimeout(function () {
-//     var div = document.getElementsByClassName('picture');
-//     for (var i = 0; i < div.length; i++) {
-//       div[i].style.display = 'none';
-//     }
-//   }, 2000);
-//   isStart = true;
- }
+  isStart = true;
+}
 
 function compare_img() {
-  if (!isStart) {
-    return;
-  }
-
-  if (count == 30) {
-    alert('다음 기회에 도전하세요');
-    isStart = false;
-    return;
-  }
-
-  var cnt = document.getElementById('count');
-  cnt.innerText = ++count;
-
-  var child_div = this.firstChild;
-  child_div.style.display = 'block';
-
-  let div = document.getElementsByClassName('picture');
-  for (var i = 0; i < div.length; i++) {
-    if (div[i] === child_div)
-      selectImg.push[i];
-  }
-
-  if (selectImg.length == 2) {
-    if (image_position[selectImg[0]] % 20 == image_position[selectImg[1]] % 20) {
-      isSame[selectImg[0]] = true;
-      isSame[selectImg[1]] = true;
-      selectImg = new Array();
-      end_count++;
-    } else {
-      setTimeout(function () {
-        selectImg = new Array();
-        let pic = document.getElementsByClassName('picture');
-        for (var i = 0; i < isSame.length; i++) {
-          if (!isSame[i]) {
-            pic[i].style.display = 'none';
-          }
-        }
-      }, 1000);
-    }
-  }
-
-  if (end_count == 10) {
-    alert('게임 끝');
-    isStart = false;
-  }
+  // Function body
 }
 
 function search_Element(obj) {
