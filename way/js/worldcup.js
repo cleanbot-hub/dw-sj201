91bae1fbde9c2f53c556a2f665e58198ede05060
cleanbot-hub <0x000000e@gmail.com>
2023-07-03 11:02:23 +0900

    main

--- way/js/worldcup.js
+++ way/js/worldcup.js
@@ -1,10 +1,14 @@
 // 기본 배열 , 모든 이미지를 다 가지고 있는 기본 배열, 실제 게임에 사용 안함 
-const image=['1.png', '2.png', '3.png', '4.png', '5.png',
-'6.png', 'goldenretriever.png', 'jindo.png', 'maltese.png', 'mixdog.png', 'papillon.png', 'pomeranian.png', 'poodle.png', 'pug.png', 'schnauzer.png', 'scottishterrier.png',
-'shibaInu.png', 'shihtzu.png', 'welshcorgi.png', 'yorkshireterrier.png','cat1.png','cat2.png','cat3.png','gom.png'
+const image=["고양이1.png","고양이2.png","고양이3.png","골든리트리버.png","곰.png","닥스훈트.png",
+"말티즈.png","믹스견.png","보더콜리.png","불독.png","비글.png","비숑.png","슈나우저.png","스코티쉬테리어.png"
+,"시바견.png","시츄.png","요크셔테리어.png","웰시코기.png","진돗개.png","치와와.png"
+,"팡피옹.png","퍼그.png","포메리안.png","푸들.png"
 ];
 
-const kind =["비글","비숑","보더콜리","골든리트리버","진돗개","말티즈","믹스견","파피옹","포메리안","푸들","퍼그","schnauzer","스코티시테리어","시바견","시츄","welshcorgi","yorkshireterrier","고양이1","고양이2","고양이3","곰"
+const kind =["고양이1","고양이2","고양이3","골든리트리버","곰","닥스훈트",
+"말티즈","믹스견","보더콜리","불독","비글","비숑","슈나우저","스코티쉬테리어"
+,"시바견","시츄","요크셔테리어","웰시코기","진돗개","치와와"
+,"팡피옹","퍼그","포메리안","푸들"
 
 ];
 
@@ -17,6 +21,17 @@ let count =1; // 현재 순서 (처음은 1번)
 
 function 순서섞기(){
 
+    for(var i=1; i<=round; i++){
+        var tmp = Math.floor(Math.random() * round); //인덱스는 0부터 시작 
+        if(순서.indexOf(tmp) == -1){
+            순서.push(tmp);
+
+        }else { 
+            --i;
+        }
+    }
+
+
 }
 
 
@@ -28,4 +43,15 @@ function 태그선택(id){
 window.onload=function(){
     var title = 태그선택("title");
     title.innerHTML=round+"강 " +count+"/"+(round/2);
+
+    순서섞기();
+    var left=태그선택("leftimg");
+    var right=태그선택("rightimg");
+    var leftText=태그선택("leftText");
+    var rightText=태그선택("rightText");
+
+    left.src="./image/"+image[순서[0]]; // image 인덱스 배열에 있는 것은 0부터 
+    right.src="./image/"+image[순서[1]]; // image
+    leftText.innerHTML=kind[순서[0]];
+    rightText.innerHTML=kind[순서[1]];
 }
