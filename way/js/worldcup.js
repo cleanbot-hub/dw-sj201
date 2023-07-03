61846c3d86f8c50934646befe2ef4de5390858cc
cleanbot-hub <0x000000e@gmail.com>
2023-07-03 09:39:05 +0900

    main

---
+++ way/js/worldcup.js
@@ -0,0 +1,13 @@
+// 기본 배열 , 모든 이미지를 다 가지고 있는 기본 배열, 실제 게임에 사용 안함 
+const image=['1.png', '2.png', '3.png', '4.png', '5.png',
+'6.png', 'goldenretriever.png', 'jindo.png', 'maltese.png', 'mixdog.png', 'papillon.png', 'pomeranian.png', 'poodle.png', 'pug.png', 'schnauzer.png', 'scottishterrier.png',
+'shibaInu.png', 'shihtzu.png', 'welshcorgi.png', 'yorkshireterrier.png','cat1.png','cat2.png','cat3.png'
+
+
+];
+
+
+let 토너먼트1=new Array(); // 현재 토너먼트
+let 토너먼트2=new Array(); // 다음 토너먼트 (현재 토너먼트에서 선택 한 것 )
+let 순서=new Array();
+let 
