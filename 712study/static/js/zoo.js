d0f0a1af596b86e4d05e41c5416fc2df9e310926
cleanbot-hub <0x000000e@gmail.com>
2023-07-12 17:05:30 +0900

    main

--- 712study/static/js/zoo.js
+++ 712study/static/js/zoo.js
@@ -1,6 +1,34 @@
- var searchButton = document.querySelector('.search_button');
-  var searchBar = document.querySelector('.search_bar');
+    /*
+    오늘의 project
+        돋보기 모양 아이콘을 눌렀을 때 , 
+            <div class="search_bar" style="display: none;">
+                        <div class="search_close">
+                            <i class="bi bi-x-circle"></i>
+                        </div>
+                        <form id="searchform" name="searchform" method="get" action="index.html">
+                            <div>
+                                <input type="text" id="s" placeholder="검색입력">
+                                <button class="srh_button">검색</button>
+                            </div>
+                        </form>
+                        해당하는 코드의 부분이 화면에 나타나야하고 , 
 
-  searchButton.addEventListener('click', function() {
-   alert("되냐");
-  });
+                    <div class="search_close">
+                            <i class="bi bi-x-circle"></i>
+                        </div>
+                        close 모양의 아이콘을 눌렀을 떄 , 창이 나오게 해야함.
+    */
+
+                        document.addEventListener('DOMContentLoaded', function() {
+                            const searchButton = document.querySelector('.search_button');
+                            const searchBar = document.querySelector('.search_bar');
+                            const searchClose = document.querySelector('.search_close');
+                        
+                            searchButton.addEventListener('click', function() {
+                                searchBar.classList.toggle('show');
+                            });
+                        
+                            searchClose.addEventListener('click', function() {
+                                searchBar.classList.remove('show');
+                            });
+                        });
