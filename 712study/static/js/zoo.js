6760a3311fb99a84d03236fdeb1f771825f43811
cleanbot-hub <0x000000e@gmail.com>
2023-07-12 13:39:22 +0900

    main

---
+++ 712study/static/js/zoo.js
@@ -0,0 +1,6 @@
+ var searchButton = document.querySelector('.search_button');
+  var searchBar = document.querySelector('.search_bar');
+
+  searchButton.addEventListener('click', function() {
+   alert("되냐");
+  });
