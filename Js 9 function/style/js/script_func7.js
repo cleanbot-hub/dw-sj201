052df1cc56e15e39ea651a6ab9f79096d63ca478
cleanbot-hub <0x000000e@gmail.com>
2023-07-14 13:29:42 +0900

    main

--- Js 9 function/style/js/script_func7.js
+++
@@ -1,9 +0,0 @@
-
-
-function input(){
-    // alert("동작"); // 4번 
-   var num=document.getElementById("num").value;
-   num=parseInt(num);
-   document.getElementById("result").innerHTML=num*num;
-}
-
