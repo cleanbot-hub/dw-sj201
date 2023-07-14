0096f399084be068d7d4a1f44f8dd006059413cf
cleanbot-hub <0x000000e@gmail.com>
2023-07-14 09:54:01 +0900

    main

--- 712study/static/js/zoo.js
+++ 712study/static/js/zoo.js
@@ -20,6 +20,30 @@
     */
 
                     window.onload=function(){
+                        
+                        var pre = document.getElementsByClassName("prev")[0];
+                        var next= document.getElementsByClassName("next")[0];
+                        pre.addEventListener("click",function(){
+                          
+
+                        
+                        });
+
+                        next.addEventListener("click",function(){
+                            var slideview = document.getElementsByClassName("eventslide")[0];
+                            var now_left = slideview.style.left.split("px")[0];
+                            if(now_left == -936) return;
+                            var left=now_left -312;
+                            slideview.style.left=left+"px";
+
+                        });
+
+                        
+                        
+                        
+                        
+                        
+                        
                         var search_bt = document.getElementsByClassName("search_button");
                         search_bt[0].addEventListener("click",function(){
                             var bar = document.querySelectorAll(".search_bar")[0];
