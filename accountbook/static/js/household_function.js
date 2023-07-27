46e807d990107b15dd2e5c0b6513c6da8b79252d
cleanbot-hub <0x000000e@gmail.com>
2023-07-27 11:19:06 +0900

    main

--- accountbook/static/js/household_function.js
+++ accountbook/static/js/household_function.js
@@ -2,5 +2,19 @@
 
 window.onload=function(){
     var today = new Date();
-    alert(today.getFullYear());
+    var year = today.getFullYear(); //년도
+    var month = today.getMonth()+1; // 월
+    var date = today.getDate(); // 일
+    var hour=today.getHours(); // 시간
+    var minute=today.getMinutes();//분
+   
+   
+    var wdate= document.querySelector("#wdate");
+    wdate.value=year+"-"+month+"-"+date+" "+hour+":"+minute;
+    
+
+
+    wdate.addEventListener("change",function(){
+        alert(this.value);
+    });
 }
