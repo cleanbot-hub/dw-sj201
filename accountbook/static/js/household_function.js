bc99403a4ff116a9a2bde6ce85c3f0d79385b666
cleanbot-hub <0x000000e@gmail.com>
2023-07-27 13:33:06 +0900

    main

--- accountbook/static/js/household_function.js
+++ accountbook/static/js/household_function.js
@@ -90,23 +90,18 @@ window.onload=function(){
 
 
         var income_bt = document.querySelector("#income_bt");
-        income_bt.addEventListener("clcik",function(){
+        income_bt.addEventListener("click",function(){
             var ex = document.getElementsByClassName("expense")[0];
             var inc = document.getElementsByClassName("income")[0];
             ex.classList.add("hide");
             inc.classList.remove("hide");
         });
-
-
-
-
+        
         var expense_bt = document.querySelector("#expense_bt");
-        expense_bt.addEventListener("clcik",function(){
+        expense_bt.addEventListener("click",function(){
             var ex = document.getElementsByClassName("expense")[0];
             var inc = document.getElementsByClassName("income")[0];
             ex.classList.remove("hide");
             inc.classList.add("hide");
-
-    });
-
+        });
 }
