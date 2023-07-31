71cc38c3f6b280c9e50a9843f8d74bc4a57106d3
cleanbot-hub <0x000000e@gmail.com>
2023-07-31 09:36:05 +0900

    main

--- 731study/static/js/jquery.js
+++ 731study/static/js/jquery.js
@@ -6,7 +6,8 @@
 $(function(){
     // document.getElementById("box1").innerHTML="<b>빵빵</b>";
     // document.querySelector("#box1").innerText="<b>빵빵아옥지얌</>";
-    $("#box1").html("<b>이야</b>");
+    $("#box1").html("<b>이야</b><br>");
+
     // $("#box1").text("이야아");
 
     // var d = document.getElementById("box1");
