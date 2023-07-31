7f52e8a90c1ff7989cc8272bd26ef0e5ccad8054
cleanbot-hub <0x000000e@gmail.com>
2023-07-31 13:12:44 +0900

    main

---
+++ 731study/static/js/input_get.js
@@ -0,0 +1,22 @@
+
+$(function(){
+
+    $("#setImage").click(function(){
+        var data= $("#imageName").val();
+        $("#gallery").append("<img src='"+data+"'>");
+       
+
+});
+
+
+
+
+
+
+})
+
+
+// append 선택한 태그의 마지막에 추가 
+// prepend 선택한 태그의 처음에 추가 
+// before 선택한 태그의 
+// after 선택한 태그의 
