aa3ab923b7cec3c5256586870a0a97163666c667
cleanbot-hub <0x000000e@gmail.com>
2023-07-31 10:58:30 +0900

    main

--- 731study/static/js/jquery.js
+++ 731study/static/js/jquery.js
@@ -3,22 +3,39 @@
 
 
 
-$(function(){
-    // document.getElementById("box1").innerHTML="<b>빵빵</b>";
-    // document.querySelector("#box1").innerText="<b>빵빵아옥지얌</>";
-    $("#box1").html("<b>보일까말까 </b><br>");
+// $(function(){
+//     // document.getElementById("box1").innerHTML="<b>빵빵</b>";
+//     // document.querySelector("#box1").innerText="<b>빵빵아옥지얌</>";
+//     // $("#box1").html("<b>보일까말까 </b><br>");
 
-    // $("#box1").text("이야아");
+//     // // $("#box1").text("이야아");
 
-    // var d = document.getElementById("box1");
-    // d.addEventListener("click",function(){});
+//     // // var d = document.getElementById("box1");
+//     // // d.addEventListener("click",function(){});
 
-    $("#box1").click(function(){
-        // $("#box2").hide(3000); 해당 태그를 감추기 
-        // 해당 태그를 보이기 
-        // $("#box2").show(3000);
-        $("#box2").toggle(2000);
+//     // $("#box1").click(function(){
+//     //     // $("#box2").hide(3000); 해당 태그를 감추기 
+//     //     // 해당 태그를 보이기 
+//     //     // $("#box2").show(3000);
+//     //     $("#box2").toggle(2000);
 
-    })
+//     // })
 
-})
+
+//     $("#fade_out").click(function(){
+//         $("#box3").fadeOut(2000);
+//     });
+//     $("#fade_in").click(function(){
+//         $("#box3").fadeIn();
+//     })
+//     $("#fade_toggle").click(function(){
+//         $("#box3").fadeToggle();
+//     })
+
+
+
+// })
+
+
+
+
