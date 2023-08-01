f6bc2e1c27901d442cf3d314427e9fcdd940fcd3
cleanbot-hub <0x000000e@gmail.com>
2023-08-01 14:45:25 +0900

    main

--- 81study/static/js/list.js
+++ 81study/static/js/list.js
@@ -3,7 +3,7 @@ $(function () {
     $("#keyword").on("keyup", function () {
         var keyword = $(this).val(); // keyword 아이디를 가져온다 
 
-       // each 선택한 요소가 여러 개 일 때 각각에 대한 함수 
+       // #data_list .data 의 선택한 요소가 여러 개 이기 때문에 each 사용  
         $("#data_list .data").each(function () {
             //attribbute 속성의 값을 제어하기 위해 attr 사용 
             $(this).toggle($(this).find("img").attr("alt").indexOf(keyword) > -1);
