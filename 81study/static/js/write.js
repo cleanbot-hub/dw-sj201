bb86a469da0f17a27dc29c3aa88b84a6baf574b7
cleanbot-hub <0x000000e@gmail.com>
2023-08-01 17:17:03 +0900

    main

--- 81study/static/js/write.js
+++ 81study/static/js/write.js
@@ -40,34 +40,45 @@
 
 
 $(function() {
+    // 이야기 필터링 함수
     function filterStories() {
-        var keyword = $("#keyword").val();
-        var selectedCategory = $("#category").val();
-        var selectedKey = $("#key").val();
+        // 검색어, 선택된 카테고리, 선택된 검색 옵션을 가져옵니다.
+        var keyword = $("#keyword").val(); // 검색어
+        var selectedCategory = $("#category").val(); // 선택된 카테고리
+        var selectedKey = $("#key").val(); // 선택된 검색 옵션
 
+        // 모든 이야기를 숨기고, 필터링 조건에 맞는 이야기만 보여줍니다.
         $(".story").hide().filter(function() {
-            var storyTitle = $(this).find("h3").text();
-            var storyContent = $(this).find("p").text();
+            var storyTitle = $(this).find("h3").text(); // 이야기 제목
+            var storyContent = $(this).find("p").text(); // 이야기 내용
 
-           
+            // 선택된 카테고리와 이야기의 제목을 비교하여 카테고리가 일치하는지 확인합니다.
             var isCategoryMatch = selectedCategory === "all" || storyTitle.startsWith(categories[selectedCategory]);
-            var isTitleMatch = selectedKey === "title" && storyTitle.indexOf(keyword) > -1;
-            var isContentMatch = selectedKey === "content" && storyContent.indexOf(keyword) > -1;
-            var isTitleOrContentMatch = selectedKey === "tc" && (storyTitle.indexOf(keyword) > -1 || storyContent.indexOf(keyword) > -1);
-            
+
+            // 선택된 검색 옵션에 따라 이야기 제목 또는 내용에 검색어가 포함되는지 확인합니다.
+            var isTitleMatch = selectedKey === "title" && storyTitle.indexOf(keyword) > -1; // 제목 검색
+            var isContentMatch = selectedKey === "content" && storyContent.indexOf(keyword) > -1; // 내용 검색
+            var isTitleOrContentMatch = selectedKey === "tc" && (storyTitle.indexOf(keyword) > -1 || storyContent.indexOf(keyword) > -1); // 제목 또는 내용 검색
+
+            // 카테고리가 일치하고 검색 옵션에 맞는 검색어가 포함되면 해당 이야기를 보여줍니다.
             return isCategoryMatch && (isTitleMatch || isContentMatch || isTitleOrContentMatch);
         }).show();
     }
 
-    
+    // 검색어 입력 상자에 키보드 이벤트 발생 시, 이야기 필터링 함수를 호출합니다.
     $("#keyword").on("keyup", filterStories);
+
+    // 카테고리 선택 상자 값이 변경되었을 때, 이야기 필터링 함수를 호출합니다.
     $("#category").on("change", filterStories);
+
+    // 검색 옵션 선택 상자 값이 변경되었을 때, 이야기 필터링 함수를 호출합니다.
     $("#key").on("change", filterStories);
 
+    // 초기에도 이야기 필터링 함수를 한번 호출하여 기본적으로 모든 이야기를 보여줍니다.
     filterStories();
 });
 
-
+// 카테고리와 해당 카테고리에 대한 한글 키워드를 매핑하는 객체입니다.
 const categories = {
     "all": "",
     "novel": "단편소설",
@@ -76,6 +87,7 @@ const categories = {
     "proverb": "속담",
     "diary": "일기"
 };
+
 
    
   
