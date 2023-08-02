0807da0ca6bc4a2ad7d0f3e3c59b3930b12abf41
cleanbot-hub <0x000000e@gmail.com>
2023-08-02 16:01:21 +0900

    main

--- 82study/static/js/chat.js
+++ 82study/static/js/chat.js
@@ -5,6 +5,6 @@ $(function(){
 
 function send(){
     var send_data = {'writer':$("#writer").val(), 'content':$("#content").val()};
-    var xmlHttp = new XMLHttpRequest();
-
-}
+    var xmlHttp = new XMLHttpRequest(); // 서버와 주고 받고 할 때 사용 되는 객체 , 새로고침 없이 데이터를 받아 올 수 있다. xml은 클라이언트와 서버 동작 할 수 있는 방식 자바스크립트에서 사용 되는 객체
+    
+}   
