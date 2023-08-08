caf5ee7585f492dddf47f383ddbe8c4002129de7
cleanbot-hub <0x000000e@gmail.com>
2023-08-08 10:51:08 +0900

    main

--- 202308/87study/js/health.js
+++ 202308/87study/js/health.js
@@ -131,20 +131,27 @@ $(function(){
                     
                     
                     $("#section").empty();
-                   $.each(data_list, function(i, item){
-                        $("#section").append(
-                            "<div class='item_short'><div class='item_image'>"+
-                            "<img src='https://loremflickr.com/200/200/health?random="+i+"'></div>"+
-                            "<div class='item_detail_box'><ul class='item_detail'>"+
-                            "<li>"+item.건강증진센터명+"</li><li>"+item.소재지도로명주소+"</li>"+
-                            "<li>"+item.건강증진업무내용+"</li><li>"+item.건강증진센터구분+"</li>"+
-                            "<li>"+item.운영기관명+"</li><li>"+item.운영기관전화번호+"</li></ul></div></div>"
-                        );
-                   });
+            
                 });
 
                 });
             });
 
 
+
+            function  view(data_list){
+
+                $.each(data_list, function(i, item){
+                    $("#section").append(
+                        "<div class='item_short'><div class='item_image'>"+
+                        "<img src='https://loremflickr.com/200/200/health?random="+i+"'></div>"+
+                        "<div class='item_detail_box'><ul class='item_detail'>"+
+                        "<li>"+item.건강증진센터명+"</li><li>"+item.소재지도로명주소+"</li>"+
+                        "<li>"+item.건강증진업무내용+"</li><li>"+item.건강증진센터구분+"</li>"+
+                        "<li>"+item.운영기관명+"</li><li>"+item.운영기관전화번호+"</li></ul></div></div>"
+                    );
+               });
+
+
+            }
 
