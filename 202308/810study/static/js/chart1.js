2ae6888e7cbed3e3ed7ac3b3f8cb8a2c071a1e72
cleanbot-hub <0x000000e@gmail.com>
2023-08-11 08:43:05 +0900

    main

--- 202308/810study/static/js/chart1.js
+++ 202308/810study/static/js/chart1.js
@@ -32,28 +32,28 @@ $(function(){
 
 
     function draw(name,tall){
-        ctx.fillStyle="#000";
-        ctx.font="20px Arial";
-        ctx.fillText(name,50+100*cnt,680);
+       ctx.beginPath();
+       ctx.moveTo(400,350);
+       ctx.arc(400,350,300,18*Math.PI/180 , 18*Math.PI/180,true);
+       ctx.fillStyle="blue";
+       ctx.fill();
 
 
-        ctx.beginPath();
-        ctx.arc(80+100*cnt,600-tall,3,0,2*Math.PI);
-        ctx.fillStyle="red";
-        ctx.fill();
+       ctx.beginPath();
+       ctx.moveTo(400,350);
+       ctx.arc(400,350,300,18*Math.PI/180 , 90*Math.PI/180,true);
+       ctx.fillStyle="green";
+       ctx.fill();
 
-        if(oldx==0 && oldy==0){
-            oldx=80+100*cnt;
-            oldy=600-tall;
-        }else{
-            ctx.moveTo(oldx,oldy);
-            ctx.lineTo(80+100*cnt,600-tall);
-            ctx.stroke();
+       ctx.beginPath();
+       ctx.moveTo(400,350);
+       ctx.arc(400,350,300,90*Math.PI/180 , 223.2*Math.PI/180,true);
+       ctx.fillStyle="yellow";
+       ctx.fill();
 
-            oldx=80+100*cnt;
-            oldy=600-tall;
-        }
-        cnt++;
-
-
+       ctx.beginPath();
+       ctx.moveTo(400,350);
+       ctx.arc(400,350,300,223.2*Math.PI/180 , 270*Math.PI/180,true);
+       ctx.fillStyle="black";
+       ctx.fill();
     }
