16755fc0939431dbe68a5e55d5f0b22b94913f92
cleanbot-hub <0x000000e@gmail.com>
2023-08-10 14:55:07 +0900

    main

---
+++ 202308/810study/static/js/fire.js
@@ -0,0 +1,64 @@
+let data = []; // json 데이터 저장할 변수 
+let fire_stat = {}; // 불이 난 지역 정보를 저장할 객체
+let ctx=""; 
+let color=["#FF6347","#FFD700","#7CFC00","#008080","#0000FF",
+"#BA55D3","#D2691E","#B0C4DE","#FF4500","#228B22"];
+
+
+async function getData() {
+    var temp = await fetch("./traffic.json").then((r) => r.json());
+    return temp.body.items;
+}
+
+
+$(async function () {
+    // Fetch data from the JSON file
+    data = await getData();
+
+    // Process the data to populate the fire_stat object
+    data.forEach(function (item) {
+        if (item.rsacGutFsttOgidNm in fire_stat) {
+            fire_stat[item.rsacGutFsttOgidNm].출동건수 += item.gutCo;
+            fire_stat[item.rsacGutFsttOgidNm].환자수 += item.trnfPcnt;
+        } else {
+            fire_stat[item.rsacGutFsttOgidNm] = {
+                출동건수: Number(item.gutCo),
+                환자수: Number(item.trnfPcnt),
+            };
+        }
+    });
+
+  var ctx = $("#graph")[0].getContext("2d");
+  var keys=Object.keys(fire_stat);
+  var oldx=oldy=0;
+  var oldtext=0;
+
+  $.each(keys,function(i,f){
+        ctx.fillStyle="#000";
+        ctx.font="15px Arial";
+        ctx.fillText(f,50+100*i,780);
+        ctx.fillStyle
+
+        ctx.fillStyle=color[i];
+        ctx.fillRect(50+100*i,760-fire_stat[f].출동건수,50,fire_stat[f].출동건수);
+
+
+        if(oldx==0 && oldy==0){
+            oldx=80+100*i;
+            oldy=760-fire_stat[f].환자수;
+        }else{
+            ctx.moveTo(oldx,oldy);
+            ctx.lineTo(80+100*i, 760-fire_stat[f].환자수);
+            ctx.stroke();
+
+            oldx=80+100*i;
+            oldy=760-fire_stat[f].환자수;
+
+
+
+        }
+
+
+  });
+
+});
