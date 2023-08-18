2a187e33daba29a91b8cd615bb938b2df0eebc11
cleanbot-hub <0x000000e@gmail.com>
2023-08-18 13:37:09 +0900

    main

--- 202308/818study/js/car.js
+++ 202308/818study/js/car.js
@@ -1,76 +1,35 @@
-let data = []; // JSON 데이터를 저장할 변수
+let data = new Object(); // 연료 유형별 데이터를 저장할 객체
+
+const types = ["하이브리드", "수소", "경유", "휘발유", "전기"]; // 연료 유형 목록
 
 async function getData() {
-  const response = await fetch("./car.json");
-  const jsonData = await response.json();
-  return jsonData;
+  var temp = await fetch("./car.json").then((res) => res.json()); // JSON 데이터 가져오기
+  console.log(temp);
+  refine(temp); // 데이터 정제 함수 호출
 }
 
-async function main() {
-  data = await getData();
-  createFuelChart();
-}
+$(async function () {
+  await getData(); // 데이터 가져오기 및 정제 함수 호출
+});
 
-function createFuelChart() {
-  const fuelTypes = [
-    '수소', '전기', '하이브리드(LPG+전기)', '하이브리드(경유+전기)', '하이브리드(휘발유+전기)',
-    '휘발유', '휘발유(무연)', '휘발유(유연)', '경유', '하이브리드(CNG+전기)'
-  ];
-
-  const fuelDataByYear = {}; // 연도별 연료 데이터를 저장할 객체
-
-  for (const car of data) {
-    const year = car.REG_YY; // 차량 등록 연도
-    const fuelType = car.DTCONT; // 연료 타입
-    const isRidingCar = car.RIDNG_ODR === '0'; // 주행 목적 여부
-
-    if (car.PURPOS_DIV === '비사업용' && isRidingCar && fuelTypes.includes(fuelType)) {
-      if (!fuelDataByYear[year]) {
-        fuelDataByYear[year] = {};
-      }
-
-      if (!fuelDataByYear[year][fuelType]) {
-        fuelDataByYear[year][fuelType] = 0;
-      }
-
-      fuelDataByYear[year][fuelType] += parseInt(car.VAN_CNT); // 차량 대수 누적
-    }
-  }
-
-  const years = Object.keys(fuelDataByYear); // 연도 목록 추출
-  const datasets = [];
-
-  for (const fuelType of fuelTypes) {
-    const dataPoints = years.map(year => fuelDataByYear[year][fuelType] || 0);
-    datasets.push({
-      label: fuelType,
-      data: dataPoints,
-      backgroundColor: getRandomColor(), // 무작위 색상 생성
-      borderWidth: 1
-    });
-  }
-
-  const ctx = document.getElementById('car').getContext('2d'); // 캔버스 요소 가져오기
-  new Chart(ctx, {
-    type: 'bar', // 막대 그래프로 변경
-    data: {
-      labels: years, // 연도별 레이블
-      datasets: datasets
-    },
-    options: {
-      responsive: true, // 반응형 설정
-      maintainAspectRatio: false // 종횡비 유지 설정
-    }
+function refine(temp) {
+  // JSON 데이터 정제 함수
+  $.each(temp, function (i, c) {
+    var type = "";
+    // 연료 유형이 types 배열에 포함되어 있는지 확인
+    if ((type = include(c.DTCONT)) == "" || c.PURPOS_DIV === "사업용") return true;
+    if (!(type in data)) data[type] = new Object(); // 연료 유형이 객체에 없으면 추가
+    if (!(c.REG_YY in data[type]))
+      data[type][c.REG_YY] = Number(c.RIDNG_ODR); // 연도가 객체에 없으면 추가
+    else data[type][c.REG_YY] += Number(c.RIDNG_ODR); // 이미 있는 연도의 데이터 누적
   });
+  console.log(data); // 정제된 데이터 출력
 }
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)]; // 무작위 색상 생성
+function include(kind) {
+  // 연료 유형이 types 배열에 포함되는지 확인하는 함수
+  for (var i in types) {
+    if (kind.indexOf(types[i]) > -1) return types[i]; // 포함되면 해당 연료 유형 반환
   }
-  return color;
+  return ""; // 포함되지 않으면 빈 문자열 반환
 }
-
-main();
