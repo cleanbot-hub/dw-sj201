c125e33bee057f7b55b56191f56dd635ea8f824e
cleanbot-hub <0x000000e@gmail.com>
2023-08-18 15:28:41 +0900

    main

--- 202308/818study/js/car.js
+++ 202308/818study/js/car.js
@@ -1,56 +1,94 @@
-let data = new Object(); // 연료 유형별 데이터를 저장할 객체
+let data=new Object();
 
-const types = ["하이브리드", "수소", "경유", "휘발유", "전기"]; // 연료 유형 목록
+const types=["하이브리드","수소","경유","휘발유","전기"];
 
-async function getData() {
-  var temp = await fetch("./car.json").then((res) => res.json()); // JSON 데이터 가져오기
-  console.log(temp);
-  refine(temp); // 데이터 정제 함수 호출
+async function getData(){
+    var temp = await fetch("./car.json").then((res)=>res.json());
+    console.log(temp);
+    refine(temp);
 }
 
-$(async function () {
-  await getData(); // 데이터 가져오기 및 정제 함수 호출
-  const ctx = $("#car")[0];
+$( async function(){
+    await getData();
+    const ctx = $("#car")[0];
 
-  new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: Object.keys(data[types[0]]).map(Number), //
-      datasets: types.map((type, index) => ({
-        label: type,
-        data: Object.values(data[type]),
-        backgroundColor: getColor(index),
-        borderColor: getColor(index),
-        fill: false,
-      })),
-    },
- 
-  });
+ var fuel = Object.keys(data);
+    var year = Object.keys(data[types[0]]);
+
+    
+    new Chart(ctx,{
+        type:"line",
+        data:{
+            labels:year,
+            datasets:
+            [
+                {
+                    label:fuel[0],
+                    data:data[fuel[0]],
+                    borderColor:"DarkTurquoise",
+                    backgroundColor:"DarkTurquoise",
+                    yAxisID:"하이브리드",
+                    fill:false
+                },
+                {
+                    label:fuel[1],
+                    data:data[fuel[1]],
+                    borderColor:"DodgerBlue",
+                    backgroundColor:"DodgerBlue",
+                    yAxisID:"수소",
+                    fill:false
+                },
+                {
+                    label:fuel[2],
+                    data:data[fuel[2]],
+                    borderColor:"Orange",
+                    backgroundColor:"Orange",
+                    yAxisID:"경유",
+                    fill:false
+                },
+                {
+                    label:fuel[3],
+                    data:data[fuel[3]],
+                    borderColor:"LightCoral",
+                    backgroundColor:"LightCoral",
+                    yAxisID:"휘발유",
+                    fill:false
+                },
+                {
+                    label:fuel[4],
+                    data:data[fuel[4]],
+                    borderColor:"SpringGreen",
+                    backgroundColor:"SpringGreen",
+                    yAxisID:"전기",
+                    fill:false
+                },
+            ]
+        },
+        options:{
+            // plugins:{legend:{labels:{font:{size:20}}}},
+            scales:{
+               
+            }
+        }
+    });
 });
-
-function getColor(index) {
-  const colors = ["red", "blue", "green", "purple", "orange"];
-  return colors[index % colors.length];
+function refine(temp){
+    $.each(temp,function(i,c){
+        var type='';
+        if( (type=include(c.DTCONT)) == '' || c.PURPOS_DIV==="사업용") return true;
+        if( !(type in data)) // 연료명으로 key가 있냐 없냐
+            data[type]=new Object();  // 없으면 연료명으로 key 생성
+        if( !( c.REG_YY in data[type]) ) // 년도로 키가 있냐 없냐
+            data[type][c.REG_YY]=Number(c.RIDNG_ODR); //없으면 년도 키 생성 하고 키에 승용차수저장
+        else
+            data[type][c.REG_YY]+=Number(c.RIDNG_ODR);//승용차수 누적
+    });
+    console.log(data);
 }
-
-function refine(temp) {
-  // JSON 데이터 정제 함수
-  $.each(temp, function (i, c) {
-    var type = "";
-    // 연료 유형이 types 배열에 포함되어 있는지 확인
-    if ((type = include(c.DTCONT)) == "" || c.PURPOS_DIV === "사업용") return true;
-    if (!(type in data)) data[type] = new Object(); // 연료 유형이 객체에 없으면 추가
-    if (!(c.REG_YY in data[type]))
-      data[type][c.REG_YY] = Number(c.RIDNG_ODR); // 연도가 객체에 없으면 추가
-    else data[type][c.REG_YY] += Number(c.RIDNG_ODR); // 이미 있는 연도의 데이터 누적
-  });
-  console.log(data); // 정제된 데이터 출력
-}
-
-function include(kind) {
-  // 연료 유형이 types 배열에 포함되는지 확인하는 함수
-  for (var i in types) {
-    if (kind.indexOf(types[i]) > -1) return types[i]; // 포함되면 해당 연료 유형 반환
-  }
-  return ""; // 포함되지 않으면 빈 문자열 반환
+function include(kind){
+    for(var i in types){
+        if(kind.indexOf(types[i]) > -1)
+            return types[i];
+    }
+    return '';
 }
