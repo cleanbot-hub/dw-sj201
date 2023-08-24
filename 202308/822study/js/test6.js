8637cac349f4de596c1e7c18690b39ca02f96115
cleanbot-hub <0x000000e@gmail.com>
2023-08-24 12:23:39 +0900

    main

--- 202308/822study/js/test6.js
+++ 202308/822study/js/test6.js
@@ -9,6 +9,8 @@ $(function() {
 });
 
 $(document).ready(function() {
+    let usedColors = new Set(); // 이미 사용된 색상을 기록하는 Set
+
     // JSON 데이터를 가져옴 (test6.json 대신 실제 데이터 또는 API 호출)
     $.getJSON('./data/test6.json', function(jsonData) {
         data = jsonData;
@@ -47,18 +49,113 @@ $(document).ready(function() {
             });
         }
 
-       // 차트 버튼 클릭 시
-$('#chart_view').on('click', function() {
-    $('#main').hide();
-    $('#chartContainer').show();
-    renderCharts(data.지출, data.수입);
-});
+        // 차트 색상을 중복되지 않게 선택하는 함수
+        function getRandomColor(usedColors) {
+            const predefinedColors = [
+                '#FF5733', '#C70039', '#900C3F', '#581845',
+                '#0074D9', '#7FDBFF', '#39CCCC', '#3D9970',
+                '#2ECC40', '#01FF70', '#FF851B', '#85144b'
+            ];
 
-// 목록 버튼 클릭 시
-$('#list_view').on('click', function() {
-    $('#chartContainer').hide();
-    $('#main').show();
-});
+            const availableColors = predefinedColors.filter(color => !usedColors.has(color));
+            const randomIndex = Math.floor(Math.random() * availableColors.length);
+            const selectedColor = availableColors[randomIndex];
+            usedColors.add(selectedColor);
+            return selectedColor;
+        }
+
+        function renderCharts(expenditures, incomes) {
+            let ctx1 = document.getElementById('chart1').getContext('2d');
+            let ctx2 = document.getElementById('chart2').getContext('2d');
+
+            // 데이터 처리 및 차트 설정
+            let expenditureDataMap = new Map();
+            expenditures.forEach(item => {
+                if (!expenditureDataMap.has(item.분류)) {
+                    expenditureDataMap.set(item.분류, 0);
+                }
+                expenditureDataMap.set(item.분류, expenditureDataMap.get(item.분류) + item.금액);
+            });
+
+            let incomeDataMap = new Map();
+            incomes.forEach(item => {
+                if (!incomeDataMap.has(item.분류)) {
+                    incomeDataMap.set(item.분류, 0);
+                }
+                incomeDataMap.set(item.분류, incomeDataMap.get(item.분류) + item.금액);
+            });
+
+            let expenditureLabels = Array.from(expenditureDataMap.keys());
+            let expenditureAmounts = Array.from(expenditureDataMap.values());
+            let incomeLabels = Array.from(incomeDataMap.keys());
+            let incomeAmounts = Array.from(incomeDataMap.values());
+
+            let expenditureBackgroundColors = expenditureAmounts.map(() => getRandomColor(usedColors));
+            let incomeBackgroundColors = incomeAmounts.map(() => getRandomColor(usedColors));
+
+            let expenditureChart = new Chart(ctx1, {
+                type: 'pie',
+                data: {
+                    labels: expenditureLabels,
+                    datasets: [{
+                        label: '지출',
+                        data: expenditureAmounts,
+                        backgroundColor: expenditureBackgroundColors,
+                        borderColor: 'rgba(255, 255, 255, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    tooltips: {
+                        callbacks: {
+                            label: function(tooltipItem, data) {
+                                let currentValue = data.datasets[0].data[tooltipItem.index];
+                                let formattedValue = new Intl.NumberFormat('ko-KR').format(currentValue);
+                                return `${data.labels[tooltipItem.index]}: ${formattedValue}원`;
+                            }
+                        }
+                    }
+                }
+            });
+
+            let incomeChart = new Chart(ctx2, {
+                type: 'pie',
+                data: {
+                    labels: incomeLabels,
+                    datasets: [{
+                        label: '수입',
+                        data: incomeAmounts,
+                        backgroundColor: incomeBackgroundColors,
+                        borderColor: 'rgba(255, 255, 255, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    tooltips: {
+                        callbacks: {
+                            label: function(tooltipItem, data) {
+                                let currentValue = data.datasets[0].data[tooltipItem.index];
+                                let formattedValue = new Intl.NumberFormat('ko-KR').format(currentValue);
+                                return `${data.labels[tooltipItem.index]}: ${formattedValue}원`;
+                            }
+                        }
+                    }
+                }
+            });
+        }
+
+        // 차트 버튼 클릭 시
+        $('#chart_view').on('click', function() {
+            $('#main').hide();
+            $('#chartContainer').show();
+            renderCharts(data.지출, data.수입);
+        });
+
+        // 목록 버튼 클릭 시
+        $('#list_view').on('click', function() {
+            $('#chartContainer').hide();
+            $('#main').show();
+        });
 
         // 입력 값이 변경될 때마다 필터링 함수 호출
         $('#word, #money, input[name="category"]').on('input change', function() {
@@ -73,89 +170,4 @@ $('#list_view').on('click', function() {
         // 초기 로드 시 전체 내역 출력
         filterAndRenderList('', Infinity, []);
     });
-
-    const usedColors = new Set();
-
-    function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        
-        do {
-            for (let i = 0; i < 6; i++) {
-                color += letters[Math.floor(Math.random() * 16)];
-            }
-        } while (usedColors.has(color));
-        
-        usedColors.add(color);
-        
-        return color;
-    }
-    
-    function renderCharts(expenditures, incomes) {
-        let ctx1 = document.getElementById('chart1').getContext('2d');
-        let ctx2 = document.getElementById('chart2').getContext('2d');
-    
-        // 데이터 처리 및 차트 설정 (예시)
-        let expenditureLabels = expenditures.map(item => item.분류);
-        let expenditureAmounts = expenditures.map(item => item.금액);
-        let incomeLabels = incomes.map(item => item.분류);
-        let incomeAmounts = incomes.map(item => item.금액);
-    
-        let expenditureBackgroundColors = expenditureAmounts.map(() => getRandomColor());
-        let incomeBackgroundColors = incomeAmounts.map(() => getRandomColor());
-    
-        let expenditureChart = new Chart(ctx1, {
-            type: 'pie',
-            data: {
-                labels: expenditureLabels,
-                datasets: [{
-                    label: '지출',
-                    data: expenditureAmounts,
-                    backgroundColor: expenditureBackgroundColors,
-                    borderColor: 'rgba(255, 255, 255, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                tooltips: {
-                    callbacks: {
-                        label: function(tooltipItem, data) {
-                            let dataset = data.datasets[tooltipItem.datasetIndex];
-                            let total = dataset.data.reduce((previousValue, currentValue) => previousValue + currentValue);
-                            let currentValue = dataset.data[tooltipItem.index];
-                            let percentage = ((currentValue / total) * 100).toFixed(2);
-                            return `${data.labels[tooltipItem.index]}: ${currentValue}원 (${percentage}%)`;
-                        }
-                    }
-                }
-            }
-        });
-    
-        let incomeChart = new Chart(ctx2, {
-            type: 'pie',
-            data: {
-                labels: incomeLabels,
-                datasets: [{
-                    label: '수입',
-                    data: incomeAmounts,
-                    backgroundColor: incomeBackgroundColors,
-                    borderColor: 'rgba(255, 255, 255, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                tooltips: {
-                    callbacks: {
-                        label: function(tooltipItem, data) {
-                            let dataset = data.datasets[tooltipItem.datasetIndex];
-                            let total = dataset.data.reduce((previousValue, currentValue) => previousValue + currentValue);
-                            let currentValue = dataset.data[tooltipItem.index];
-                            let percentage = ((currentValue / total) * 100).toFixed(2);
-                            return `${data.labels[tooltipItem.index]}: ${currentValue}원 (${percentage}%)`;
-                        }
-                    }
-                }
-            }
-        });
-    }
 });
